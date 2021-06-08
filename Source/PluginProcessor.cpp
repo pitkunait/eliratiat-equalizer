6d3603d98c5bfd73c9c70233c15683c343a5a74a
Vassili Kiritsenko <vassili.kiritsenko@ericsson.com>
2021-06-08 08:38:02 +0300

    setup params

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -12,127 +12,114 @@
 //==============================================================================
 VashEQAudioProcessor::VashEQAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+        : AudioProcessor(BusesProperties()
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+                                 .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+                                 .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+)
 #endif
 {
 }
 
-VashEQAudioProcessor::~VashEQAudioProcessor()
-{
+VashEQAudioProcessor::~VashEQAudioProcessor() {
 }
 
 //==============================================================================
-const juce::String VashEQAudioProcessor::getName() const
-{
+const juce::String VashEQAudioProcessor::getName() const {
     return JucePlugin_Name;
 }
 
-bool VashEQAudioProcessor::acceptsMidi() const
-{
-   #if JucePlugin_WantsMidiInput
+bool VashEQAudioProcessor::acceptsMidi() const {
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
-bool VashEQAudioProcessor::producesMidi() const
-{
-   #if JucePlugin_ProducesMidiOutput
+bool VashEQAudioProcessor::producesMidi() const {
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
-bool VashEQAudioProcessor::isMidiEffect() const
-{
-   #if JucePlugin_IsMidiEffect
+bool VashEQAudioProcessor::isMidiEffect() const {
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
-double VashEQAudioProcessor::getTailLengthSeconds() const
-{
+double VashEQAudioProcessor::getTailLengthSeconds() const {
     return 0.0;
 }
 
-int VashEQAudioProcessor::getNumPrograms()
-{
+int VashEQAudioProcessor::getNumPrograms() {
     return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    // so this should be at least 1, even if you're not really implementing programs.
 }
 
-int VashEQAudioProcessor::getCurrentProgram()
-{
+int VashEQAudioProcessor::getCurrentProgram() {
     return 0;
 }
 
-void VashEQAudioProcessor::setCurrentProgram (int index)
-{
+void VashEQAudioProcessor::setCurrentProgram(int index) {
 }
 
-const juce::String VashEQAudioProcessor::getProgramName (int index)
-{
+const juce::String VashEQAudioProcessor::getProgramName(int index) {
     return {};
 }
 
-void VashEQAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
+void VashEQAudioProcessor::changeProgramName(int index, const juce::String &newName) {
 }
 
 //==============================================================================
-void VashEQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
+void VashEQAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock) {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
 }
 
-void VashEQAudioProcessor::releaseResources()
-{
+void VashEQAudioProcessor::releaseResources() {
     // When playback stops, you can use this as an opportunity to free up any
     // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool VashEQAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
+
+bool VashEQAudioProcessor::isBusesLayoutSupported(const BusesLayout &layouts) const {
+#if JucePlugin_IsMidiEffect
     juce::ignoreUnused (layouts);
     return true;
-  #else
+#else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
     // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+#if !JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
-
-    return true;
-  #endif
-}
 #endif
 
-void VashEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
-{
+    return true;
+#endif
+}
+
+#endif
+
+void VashEQAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer, juce::MidiBuffer &midiMessages) {
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
     // In case we have more outputs than inputs, this code clears any output
@@ -142,7 +129,7 @@ void VashEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce:
     // when they first compile a plugin, but obviously you don't need to keep
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+        buffer.clear(i, 0, buffer.getNumSamples());
 
     // This is the place where you'd normally do the guts of your plugin's
     // audio processing...
@@ -150,42 +137,80 @@ void VashEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce:
     // the samples and the outer loop is handling the channels.
     // Alternatively, you can process the samples with the channels
     // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+    for (int channel = 0; channel < totalNumInputChannels; ++channel) {
+        auto *channelData = buffer.getWritePointer(channel);
 
         // ..do something to the data...
     }
 }
 
 //==============================================================================
-bool VashEQAudioProcessor::hasEditor() const
-{
+bool VashEQAudioProcessor::hasEditor() const {
     return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* VashEQAudioProcessor::createEditor()
-{
-    return new VashEQAudioProcessorEditor (*this);
+juce::AudioProcessorEditor *VashEQAudioProcessor::createEditor() {
+//    return new VashEQAudioProcessorEditor(*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
+
 //==============================================================================
-void VashEQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
-{
+void VashEQAudioProcessor::getStateInformation(juce::MemoryBlock &destData) {
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
 }
 
-void VashEQAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
-{
+void VashEQAudioProcessor::setStateInformation(const void *data, int sizeInBytes) {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
 }
 
+juce::AudioProcessorValueTreeState::ParameterLayout VashEQAudioProcessor::createParameterLayout() {
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq",
+                                                           "LowCut Freq",
+                                                           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+                                                           20.f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq",
+                                                           "HighCut Freq",
+                                                           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+                                                           20000.f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Freq",
+                                                           "Peak Freq",
+                                                           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+                                                           750.f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Gain",
+                                                           "Peak Gain",
+                                                           juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f),
+                                                           750.f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Quality",
+                                                           "Peak Quality",
+                                                           juce::NormalisableRange<float>(0.1f, 10.f, 0.5f, 1.f),
+                                                           1.f));
+
+    juce::StringArray stringArray;
+    for (int i = 1; i < 5; i++) {
+        juce::String str;
+        str << i * 12;
+        str << " db/Oct";
+        stringArray.add(str);
+    }
+
+    layout.add(std::make_unique<juce::AudioParameterChoice>("LowCut Slope", "LowCut Slope", stringArray, 0));
+    layout.add(std::make_unique<juce::AudioParameterChoice>("HighCut Slope", "HighCut Slope", stringArray, 0));
+
+    return layout;
+}
+
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter() {
     return new VashEQAudioProcessor();
 }
